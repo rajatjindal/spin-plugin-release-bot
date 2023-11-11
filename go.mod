14b13370d262ef2e884b1df379b57b6150c84a73
Rajat Jindal <rajatjindal83@gmail.com>
2023-11-11 10:04:25 +0530

    add logs
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/fermyon/spin/sdk/go/v2 v2.0.1
 	github.com/google/go-github/v56 v56.0.0
 	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.4-0.20230606125235-dd1b4c2e81af
 	golang.org/x/oauth2 v0.6.0
 )
 
@@ -14,6 +15,7 @@ require (
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	spinhttp "github.com/fermyon/spin/sdk/go/v2/http"
 	"github.com/fermyon/spin/sdk/go/v2/variables"
 	"github.com/rajatjindal/spin-plugin-release-bot/pkg/releaser"
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		logrus.Info("starting spin-plugin-release-bot handler")
 		token, err := variables.Get("gh_token")
 		if err != nil {
-			http.Error(w, "internal server error", http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("internal server error %v", err), http.StatusInternalServerError)
 			return
 		}
 
 		rh := releaser.New(r.Context(), token)
 		rh.HandleActionWebhook(w, r)
+		logrus.Info("done spin-plugin-release-bot handler")
 	})
 }
 
